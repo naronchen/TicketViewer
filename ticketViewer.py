38ceef003451fb4223dcf1ef260dd57e5d9f663a
naronchen <naron.chen@richmond.edu>
2021-11-27 19:19:42 -0500

    Added Loginn Page & added exceptions for Login

--- ticketViewer.py
+++ ticketViewer.py
@@ -8,25 +8,39 @@ import stdiomask
 #simpe check whether response is valid
 def ResponseCheck(response):
     if response.status_code != 200:
-        print('*********** Status:', response.status_code, ', Problem with request, Exiting ***********')
+        print(f"** Status: {response.status_code}, Problem with request, Exiting **")
         if response.status_code == 401:
-            print("*********** Error Detail: It seemed like you entered an invalid email or password ***********")
+            print("** Error Detail: It seemed like you entered an invalid email or password **")
+        if response.status_code == 404:
+            print("** Error Detail: It seemed like server can't find the requested resource, check your subdomain name **")
         exit()
 
-#return data of a single page in dictionary format
+
+
+
 def GetPageData(url, user, pwd):
     # HTTP get request
-    response = requests.get(url, auth = (user, pwd))
+    try:
+        response = requests.get(url, auth = (user, pwd))
+    except requests.exceptions.ConnectionError:
+        print("** API unavailable, please check your internet connection **")
+        exit()
+
     ResponseCheck(response)
 
     #decode json response into a dictionary and use the data
     return (response.json())
+
+
+
 
 #print the subset information 
 def showInfo(df, start = 0,end = 1):
     df_sub = df[start:end]
     h = ['ticket_id','time_created', 'time_updated', 'subject']
     print(tabulate(df_sub, headers=h, tablefmt='psql', showindex=False))
+
+
 
 
 
@@ -39,6 +53,9 @@ def convertData(ticketData):
             ticket_subset[key].append(ticket[key])
     
     return(pd.DataFrame(ticket_subset))
+
+
+
 
 def updateData(s, e, user_input, df):
     status = True
@@ -57,54 +74,85 @@ def updateData(s, e, user_input, df):
         invalid()
     return s, e
 
-def invalid():
-    print('*********** Oops, you just entered sth invalid, please try again ***********\n')
+
+
 
 def checkPageLimit(df, i):
     return True if i >= 0 and i <= len(df.index) else False 
+
+
+def invalid(): print('*********** Oops, you just entered sth invalid, please try again ***********\n')
+
 
 
 
 def main():
 
     #set request params
-    url = 'https://zccnaron.zendesk.com/api/v2/incremental/tickets/cursor.json?&start_time=0'
-    user = 'naron.chen@richmond.edu'
-    pwd = 'blank'
-    
-    # user = input("--Please Enter Your Email: ")
-    # pwd = stdiomask.getpass("--Please Enter Your Password: ") # It will ask to enter password and display * on the screen
-    
-    #Get the data of all tickets, convert to pandas dataframe
+    # url = 'https://zccnaron.zendesk.com/api/v2/incremental/tickets/cursor.json?&start_time=0'
+    # user = 'naron.chen@richmond.edu'
+    # pwd = 'blank'
+
+
+    #Log In
+    while True:
+        print("\n----------------Log In-----------------")
+        print("Enter 1 to use password to log in")
+        print("Enter 2 to use API token to log in")
+        print("Enter quit to quit program")       
+
+        user_input = input()
+        if user_input == 'quit': exit()
+
+        print("\nNote: sub domain name would be whatever is in the blank of ''https://_______.zendesk.com'' ")
+        subDomain = input("--Please Enter Your sub domain Name: ")
+        url = f"https://{subDomain}.zendesk.com/api/v2/incremental/tickets/cursor.json?&start_time=0"
+        user = input("--Please Enter Your Email: ")
+
+        if user_input == '1':
+            pwd = stdiomask.getpass("--Please Enter Your Password: ") # It will ask to enter password and display * on the screen
+            break
+        if user_input == '2':
+            user += '/token'
+            pwd = stdiomask.getpass("--Please Enter Your API Token: ")
+            break
+
+        else:
+            invalid()
+
+
 
     ticketData = GetPageData(url, user, pwd) #Empty Data Exception
-    df = convertData(ticketData)
+    df = convertData(ticketData) #Get the data of all tickets, convert to pandas dataframe
     print("--Connected!")
 
 
 
+
+    #Home
     while True:
-        print("")
-        print("----------------HOME PAGE-----------------")
+        print("\n----------------HOME PAGE-----------------")
         print("     *Enter 1 to view all tickets")
         print("     *Enter 2 to view one ticket ")
         print("     *Enter quit to quit Program")
-        user_input = input()
+        user_input = input().lower()
 
+
+        #View All Tickets
         if user_input == '1':
             start, end = 0, 25
             status = True
 
             while status:
                 showInfo(df, start, end) 
-                print("--You are at Page:", end//25 )
+                print(f"--You are at Page: {end//25}" )
                 print("")
                 print("----------------VIEW ALL TICKETS-----------------")
                 print("     *Enter 1 to go to the Previous Page")
                 print("     *Enter 2 to go to the Next Page")
                 print("     *Enter home to exit to home page")
                 print("     *Enter quit to quit Program")
-                user_input = input()
+                user_input = input().lower()
 
                 if user_input == 'home': status = False
                 if user_input == 'quit': exit()
@@ -113,14 +161,14 @@ def main():
                 start, end = updateData(start, end, user_input, df) 
 
                 
-        
+        #View One Ticket
         elif user_input == '2':
             status, invalidInput = True, False
             while status:
-                if invalidInput == False: 
+                if invalidInput is False: 
                     try:
                         ticket_num = int(input("--Enter Ticket Number: "))
-                        if ticket_num == 0 or checkPageLimit(df, ticket_num) == False:
+                        if ticket_num == 0 or checkPageLimit(df, ticket_num) is False:
                             print("** ID Entered Out of Range, Please Enter a Valid ID **")
                             continue
                     except ValueError:
@@ -134,7 +182,7 @@ def main():
                 print("     *Enter quit to quit Program")
                 # if ticket_num > count or ticket_num < count:
 
-                user_input = input()
+                user_input = input().lower()
                 if user_input == '1': invalidInput = False
                 elif user_input == 'home': status = False
                 elif user_input == 'quit': exit()
@@ -147,6 +195,8 @@ def main():
         else:
             print("="*100, "\n"*2) #format
             invalid()
+
+
 
 
 main()
